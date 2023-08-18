5a32831d2c0f892bd502e306cc701ccfb4495f7c
Davidmenq <davidmenq@hotmail.com>
2023-08-17 23:06:44 -0500

    ejecutando makemigrations

--- dulceria/gestion/models.py
+++ dulceria/gestion/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+class CategoriModel(models.Model):
+
+    opcionesNivelAzucar = (
+        ['MA', 'MUY_ALTO'],
+        ['ALTO', 'ALTO'],
+        ['MEDIO', 'MEDIO'],
+        ['BAJO', 'BAJO'],
+        ['MUY_BAJO', 'MUY_BAJO'],
+        ['CERO', 'CERO']
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    nombre = models.TextField(null=False)
+    nivelAzucar = models.TextField(name='nivel_azucar', null=False, choices=opcionesNivelAzucar)
+
+    class Meta:
+        db_table = 'categorias'
+
+class GolosinaModel(models.Model):
+    tipoProcedencia = (
+        ['NACIONAL', 'NACIONAL'],
+        ['IMPORTADO', 'IMPORTADO']
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    nombre = models.TimeField(null=False)
+    fechaVencimiento = models.DateField(editable=False, null=False, name='fecha_vencimiento')
+    precio = models.FloatField(null=False)
+    procedencia = models.TextField(choices=tipoProcedencia, default='NACIONAL')
+
+    categoria = models.ForeignKey(to=CategoriModel, db_column='categoria_id', on_delete=models.PROTECT, 
+                                  related_name='golosinas')
+    
+    class Meta:
+        db_table = 'golosinas'
+        unique_together = [['nombre', 'fecha_vencimiento']]
