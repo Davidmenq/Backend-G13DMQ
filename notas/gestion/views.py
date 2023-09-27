5692815ee829a4291ae624a55f392024a8a9c376
Davidmenq <davidmenq@hotmail.com>
2023-09-26 22:47:21 -0500

    ultimos cambios AWS

--- notas/gestion/views.py
+++ notas/gestion/views.py
@@ -6,7 +6,9 @@ from .serializers import *
 from .models import *
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
-
+from boto3 import session
+from os import environ
+from django.conf import settings
 
 # Create your views here.
 class Prueba(APIView):
@@ -69,11 +71,24 @@ class NotaController(APIView):
     permission_classes=[IsAuthenticated]
     
     def post(self, request:Request):
-        print('ingreso')
-
+        print('paso')
         serializador = NotaSerializer(data=request.data)
+        print(request.user)
+        print(request.user.nombre)
+        usuarioLogeado: Usuario = request.user
+        print('_____')
+        print(request.auth)
+
         if serializador.is_valid():
-            pass
+            nuevaNota =Nota(usuario= usuarioLogeado, **serializador.validated_data)
+            nuevaNota.save()
+
+            if nuevaNota.imagen:
+                subir_imagen_s3('{}/media/{}'.format(settings.BASE_DIR, nuevaNota))
+
+            return Response(data={
+                'message': 'Nota creada exitosamente'
+            }, status=201)
         else:
             return Response(data={
                 'message':'Error al crear la nota',
@@ -82,4 +97,36 @@ class NotaController(APIView):
 
     def get(self, request):
         pass
+
+def subir_imagen_s3(nombre_imagen):
+    if nombre_imagen in None:
+        return 
+    
+    
+    nuevaSesion=session.Session(aws_access_key_id=environ.get('AWS_ACCESS_KEY'), 
+                                 aws_secret_access_key=environ.get('AWS_SECRET_KEY'),
+                                   region_name=environ.get('AWS_BUCKET_REGION'))
+    s3Client = nuevaSesion.client('s3')
+    bucket=environ.get('AWS_BUCKET_NAME')
+    with open(nombre_imagen, 'rb') as archivo:
+
+        object_name=''
+        
+        try:    
+            s3Client.upload_file(nombre_imagen, bucket, object_name)
+        except Exception as e:
+            print(e)
+
+def devolver_url_firmada(nombre_imagen):
+        if nombre_imagen in None:
+            return 
+
+        nuevaSesion=session.Session(aws_access_key_id=environ.get('AWS_ACCESS_KEY'), 
+                                 aws_secret_access_key=environ.get('AWS_SECRET_KEY'),
+                                   region_name=environ.get('AWS_BUCKET_REGION'))
+        
+        bucket=environ.get('AWS_BUCKET_NAME')
+        s3Client = nuevaSesion.client('s3')
+        
+        
 
